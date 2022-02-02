b25f9bd88f80cb953303b5304d914cce718f3b53
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-02-01 20:26:44 -0500

    Homework1 Collin Drake

---
+++ Homework1/Problem2.java
@@ -0,0 +1,14 @@
+package Homework1;
+public class Problem2 {
+    public static void main(String[] args){
+        //Initialize each variable
+        double velocity0 = 4.6;
+        double velocity1 = 11.5;
+        double time = 0.6;
+        double a;
+        //calculate average acceleration using the initialized variables
+        a = (velocity1 - velocity0)/time;
+        //print the answer and a closing statement
+        System.out.println("The average acceleration for the equation is " + a + " meters per second");
+    }
+}
