157daed89ae9430ed11d8c9d7c53f8863e21918c
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-02-03 17:52:21 -0500

    Homework1 actual

---
+++ Problem1.java
@@ -0,0 +1,13 @@
+public class Problem1 {
+    public static void main(String[] args){
+        //Initialize each variable
+        int a = 2;
+        int b = 5;
+        int c = 4;
+        int answer;
+        answer = (b * b) - (4 * a * c);
+        //print out result as well as closing statment
+        System.out.println("The discriminant of the qaudratic equation  2x^2 + 5x + 4 = 0 is " + answer);
+
+    }
+}
---
+++ Problem3.java
@@ -0,0 +1,10 @@
+public class Problem3 {
+    public static void main(String[] args){
+        //print out stars on each line to portray a 103
+        System.out.println("* ***** *****");
+        System.out.println("* *   *     *");
+        System.out.println("* *   * *****");
+        System.out.println("* *   *     *");
+        System.out.println("* ***** *****");
+    }
+}
